3a9b9e4afea5d0c5a913c71edede4d068d2462f5
cegarc5579 <carlos_garcia1@baylor.edu>
2022-03-15 18:33:25 -0500

    added for row in weather_file

--- deathvalleysitka.py
+++ deathvalleysitka.py
@@ -24,4 +24,25 @@ for index, column_header in enumerate(header_row2):
     print(index, column_header)
 highs1 = []
 lows1 = []
-dates = []
+dates1 = []
+
+# use this structure!!
+for row in weather_file:
+
+    try:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[4])
+        low = int(row[5])
+
+    except ValueError:
+        # this is a new way to print something else
+        # putting an f in the beginning allows you to put curly brackets and anything else
+        print(f"Missing data for {current_date}")
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(current_date)
+
+
+print(highs)
+print(dates)
