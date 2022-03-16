bbfb4121aeab2cbc4545045393d451339b5784cd
cegarc5579 <carlos_garcia1@baylor.edu>
2022-03-16 00:36:20 -0500

    making the sitka temp chart

--- deathvalleysitka.py
+++ deathvalleysitka.py
@@ -2,48 +2,35 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+# deathvalley = open("death_valley_2018_simple.csv",'r')
 
-# death valley temps
-weather = open("death_valley_2018_simple.csv", "r")
-weather_file = csv.reader(weather, delimiter=",")
-header_row = next(weather_file)
+sitka = "sitka_weather_2018_simple.csv"
+with open(sitka) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
 
-
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
-highs = []
-lows = []
-dates = []
-indexes = []
-
-# sitka temps
-weather2 = open("sitka_weather_2018_simple.csv", "r")
-weather_file2 = csv.reader(weather2, delimiter=",")
-header_row2 = next(weather_file2)
-
-for index, column_header in enumerate(header_row2):
-    print(index, column_header)
-highs1 = []
-lows1 = []
-dates1 = []
-
-# use this structure!!
-for row in weather_file:
-
-    try:
+    dates, highs, lows = [], [], []
+    for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[4])
-        low = int(row[5])
-
-    except ValueError:
-        # this is a new way to print something else
-        # putting an f in the beginning allows you to put curly brackets and anything else
-        print(f"Missing data for {current_date}")
-    else:
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
         highs.append(high)
         lows.append(low)
-        dates.append(current_date)
-
-
 print(highs)
-print(dates)
+
+
+# making the chart for sitke
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high temperatures", fontsize=20)
+plt.xlabel("", fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
